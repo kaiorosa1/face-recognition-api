cff38296977399a6eb8f323bec0f94bc714d25d3
kaiorosa1 <kaiosrosa@gmail.com>
2020-01-09 09:54:28 -0300

    add image and profile routes

--- script.js
+++ script.js
@@ -4,22 +4,37 @@ const app = express();
 
 const database = [
     {
+        "id": "123",
         "email": "john@gmail.com",
         "password": "cool",
         "name": "John",
-        "entries": 0
+        "entries": 0,
+        "joined": new Date()
     },
     {
+        "id": "124",
         "email": "brenda@gmail.com",
         "password": "bunnies",
         "name": "Brenda",
-        "entries": 0
+        "entries": 0,
+        "joined": new Date()
     }
 ]
 app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.send("Getting the root...");
+});
+
+app.get('/profile/:id',(req, res)=>{
+    const {id} = req.params;
+
+    database.forEach(user =>{
+        if(user.id === id){
+            return res.json(user);
+        }
+    });
+    res.status('404').json('user not found');
 });
 
 app.post('/signin',(req,res)=>{
@@ -33,12 +48,26 @@ app.post('/signin',(req,res)=>{
 app.post('/register',(req,res)=>{
     const {email, password, name} = req.body;
     database.push( {
+        "id": "134",
         "email": email,
         "password": password,
         "name": name,
-        "entries": 0
+        "entries": 0,
+        "joined": new Date()
     });
     res.json(database[database.length-1]);
+});
+
+app.post('/image',(req,res)=>{
+    const {id} = req.params;
+
+    database.forEach(user =>{
+        if(user.id === id){
+            user.entries++;
+            return res.json(user.entries);
+        }
+    });
+    res.status('404').json('not found');
 });
 
 app.listen(3000,()=>{
